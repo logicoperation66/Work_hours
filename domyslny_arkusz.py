b4bc443deffac239413edd580e8ab3fee0650d05
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-09-13 22:09:40 +0200

    kod umożliwiający tworzenie domyślnego arkusza

--- domyslny_arkusz.py
+++ domyslny_arkusz.py
@@ -1,25 +1,45 @@
-import datetime
 from datetime import *
-from tkinter import *
 from openpyxl import *
 
-### W tym pliku zajmę się utworzeniem domyślengo pustego arkusza takiego jaki dostajemy od Szefa.
-
-#Tworzymy skoroszyt(wokrbook)
-wb = Workbook()
-
-#Następnie aktywujemy pierwszy arkusz kaukulacyjny naszego skoroszytu
-sheet = wb.active
-
-data = date.today()
-start = input("Podaj start pracy w formacie H:M")
-stop = input("Podaj koniec pracy w formacie H:M")
-
+def days_cur_month():
+    """Listing all days in curent month"""
+    m = datetime.now().month
+    y = datetime.now().year
+    ndays = (date(y, m+1, 1) - date(y, m, 1)).days
+    d1 = date(y, m, 1)
+    d2 = date(y, m, ndays)
+    delta = d2 - d1
+    list_days = [(d1 + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(delta.days + 1)]
+    return list_days
 
 
-sheet.append((data,start, stop))
+def blank_sheet_create(name='Adam Wawrzyniak'):
+    """Creating a blank sheet with name, curent month and all days in month"""
+    wb = Workbook()
+    sheet = wb.active
+    sheet["A1"] = name
+    sheet["A2"] = datetime.today().strftime("%m-%y")
+    sheet["B2"] = 'suma godzin'
+    days_cur_month()
+    n = 3
+    for day in days_cur_month():
+        sheet.cell(row=n, column=1).value = day
+        n += 1
+    file = wb.save("new_file.xlsx")
+    return file
+
+start_prompt = "Witaj w aplikacji do zapisywania i obliczania czasu pracy.\n" \
+               "Wybierz\n\t1-Aby utworzyć nowy arkusz.\n\t2-Aby edytować " \
+               "istniejący."
 
 
+print(start_prompt)
+
+chose = int(input(':'))
+
+if chose == 1:
+    #name = input("Podaj swoje imie i nazwisko")
+    blank_sheet_create()
 
 
 
